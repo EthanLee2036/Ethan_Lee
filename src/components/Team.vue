53c90326576bfc400d859235dd30dc8c591933de
EthanLee2036 <ethanlee2036@gmail.com>
2025-12-01 21:36:29 +0000

    Update Team.vue

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -115,7 +115,7 @@ export default {
         ]
       },
       dataSpecialists:[
-        {name: 'Li Xun (Ethan)', degree: 'MSc in Data Science and AI', institution: 'Oxford Brookes University, United Kingdom\nLeads advanced data science and AI projects, and oversees the lab\'s computational and web infrastructure'}
+        {name: 'Li Xun (Ethan)', degree: 'MSc in Data Science and AI', institution: 'Oxford Brookes University, United Kingdom<br><p style="font-style: italic">Leads advanced data science and AI projects, and oversees the lab\'s computational and web infrastructure</p>'}
       ],
       collaborators: [
         { name: 'Annushiah Vasan Thakumar', degree: 'PhD', institution: 'Taylor\'s University, Malaysia' },
